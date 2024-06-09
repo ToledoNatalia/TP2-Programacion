5dfffa7c4802f15684d6df802c4dba5947859214
ToledoNatalia <toledonatalia2108@gmail.com>
2024-06-08 22:30:30 -0300

    subiendo proyecto

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-import './index.css'
+import "./index.css";
 
+import { CssBaseline } from "@mui/material";
+import { SnackbarProvider } from "notistack";
 
-
-import {CssBaseline} from '@mui/material';
-import {SnackbarProvider} from "notistack";
-
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <CssBaseline />
     <SnackbarProvider>
-    <CssBaseline/>
-    <App />
+      <App />
     </SnackbarProvider>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
